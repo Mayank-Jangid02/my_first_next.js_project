9052ba89498191123acf387032ea5723556dbebc
Mayank Jangid <mayankjangid0105@gmail.com>
2026-07-11 20:48:47 +0530

    use the react cache

---
+++ app/visual/page.tsx
@@ -0,0 +1,58 @@
+import { ReactFlow } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import {cache} from "react"
+const nodes = [
+  {
+    id: "1",
+    position: { x: 100, y: 100 },
+    data: { label: "Start" },
+  },
+  {
+    id: "2",
+    position: { x: 300, y: 100 },
+    data: { label: "End" },
+  },
+  {
+    id: "3",
+    position: { x: 300, y: 100 },
+    data: { label: "middle" },
+  }
+];
+
+
+const getdata=cache(async()=>{ 
+  let res= await fetch('https://jsonplaceholder.typicode.com/posts');
+  console.log(res);
+
+});
+
+const edges = [
+  {
+    id: "e1-2",
+    source: "1",
+    target: "2",
+  },
+  {
+    id: "e2-1",
+    source: "2",
+    target: "1",
+  }
+  ,{
+    id: "e3-1",
+    source: "3",
+    target: "1",
+  }
+];
+
+export default function App() {
+  getdata();
+  getdata();
+  return (
+    // <div style={{ width: "100vw", height: "100vh" }}>
+    //   <ReactFlow nodes={nodes} edges={edges} />
+    // </div>
+    <div>
+   <h1>hello visual</h1>
+    </div>
+  );
+}
